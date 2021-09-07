cb2d5644adf6eacaeee1d373781da4dec3a81c90
nada <nada3594@aucegypt.edu>
2021-09-07 19:04:42 +0200

    added files

---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(float _length, float _width)
+{
+
+}
+float Rectangle::area()
+{
+	return 0;
+}
+float Rectangle::perimeter()
+{
+	return 0;
+}
---
+++ triangle.cpp
@@ -0,0 +1,14 @@
+#include "Triangle.h"
+
+Triangle::Triangle(float _right, float _left, float _base, float _height)
+{
+
+}
+float Triangle::area()
+{
+	return 0;
+}
+float Triangle::perimeter()
+{
+	return 0;
+}
